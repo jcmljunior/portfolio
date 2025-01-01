c53a790e30b92ad64a32d88a1d6b07c2bacb3696
Julio Cesar <jcmljunior@proton.me>
2024-12-31 23:00:39 -0300

    Melhorias de SEO.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="primary-container container-fluid">
-        <h2 class="title">Minhas habilidades</h2>
+    <section class="primary-container container-fluid" itemscope itemtype="https://schema.org/DefinedTermSet">
+        <h2 class="title" itemprop="name">Minhas habilidades</h2>
         <p class="description">Embora algumas tecnologias não estejam presentes adiante, isso não significa que eu não
             tenha tido contato
             com elas. Ao decorrer de meus estudos. Optei por destacar as que considero mais relevantes para os tipos de
@@ -8,42 +8,42 @@
             conforme as necessidades de cada projeto.</p>
         <div>
             <ul class="row justify-center">
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'html5']" class="icon" />
-                    <h3 class="title">HTML5</h3>
+                    <h3 class="title" itemprop="name">HTML5</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'css3']" class="icon" />
-                    <h3 class="title">CSS3</h3>
+                    <h3 class="title" itemprop="name">CSS3</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'flutter']" class="icon" />
-                    <h3 class="title">Flutter</h3>
+                    <h3 class="title" itemprop="name">Flutter</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'js']" class="icon" />
-                    <h3 class="title">JavaScript</h3>
+                    <h3 class="title" itemprop="name">JavaScript</h3>
                 </li>
 
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'vuejs']" class="icon" />
-                    <h3 class="title">Vue.js</h3>
+                    <h3 class="title" itemprop="name">Vue.js</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'node']" class="icon" />
-                    <h3 class="title">Node.js</h3>
+                    <h3 class="title" itemprop="name">Node.js</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fas', 'database']" class="icon" />
-                    <h3 class="title">MySQL</h3>
+                    <h3 class="title" itemprop="name">MySQL</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'git']" class="icon" />
-                    <h3 class="title">Git</h3>
+                    <h3 class="title" itemprop="name">Git</h3>
                 </li>
-                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+                <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12" itemscope itemtype="https://schema.org/DefinedTerm">
                     <font-awesome-icon :icon="['fab', 'linux']" class="icon" />
-                    <h3 class="title">Linux</h3>
+                    <h3 class="title" itemprop="name">Linux</h3>
                 </li>
             </ul>
         </div>
