2d3c4781a874c994ed54f46aff4a7ff532a30a5f
Julio Cesar <jcmljunior@proton.me>
2024-12-31 22:22:07 -0300

    Remoção de estrela.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -9,22 +9,18 @@
         <div>
             <ul class="row justify-center">
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fab', 'html5']" class="icon" />
                     <h3 class="title">HTML5</h3>
                 </li>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fab', 'css3']" class="icon" />
                     <h3 class="title">CSS3</h3>
                 </li>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fab', 'flutter']" class="icon" />
                     <h3 class="title">Flutter</h3>
                 </li>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fab', 'js']" class="icon" />
                     <h3 class="title">JavaScript</h3>
                 </li>
@@ -34,12 +30,10 @@
                     <h3 class="title">Vue.js</h3>
                 </li>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fab', 'node']" class="icon" />
                     <h3 class="title">Node.js</h3>
                 </li>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fas', 'database']" class="icon" />
                     <h3 class="title">MySQL</h3>
                 </li>
@@ -48,7 +42,6 @@
                     <h3 class="title">Git</h3>
                 </li>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                    <font-awesome-icon :icon="['fas', 'star']" class="star" />
                     <font-awesome-icon :icon="['fab', 'linux']" class="icon" />
                     <h3 class="title">Linux</h3>
                 </li>
@@ -105,15 +98,6 @@
     font-size: 3rem;
 }
 
-.primary-container li .star {
-    position: absolute;
-    top: -.75rem;
-    right: -.75rem;
-    font-size: 1.5rem;
-    color: var(--star);
-    text-shadow: 0 1px rgba(0, 0, 0, .3);
-}
-
 .primary-container li .title {
     margin-top: 1rem;
     text-shadow: 0 1px rgba(0, 0, 0, .3);
@@ -121,9 +105,5 @@
 
 .primary-container li:hover .title {
     text-shadow: none;
-}
-
-.primary-container li:hover .star {
-    color: var(--inverse-star);
 }
 </style>
